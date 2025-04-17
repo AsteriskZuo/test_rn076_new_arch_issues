921c7a3ab5df96d1967cca1f22542805035e2f71
asteriskzuo <zuoyuhsywn@hotmail.com>
2025-04-17 14:45:36 +0800

    chore: update

--- App.tsx
+++ App.tsx
@@ -7,32 +7,45 @@
  */
 
 import * as React from 'react';
-import { Pressable, SafeAreaView, Text, View } from 'react-native';
 import {
+  Platform,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {
+  Chatroom,
   Container,
-  ConversationDetail,
   TextInput,
-  useChatContext,
-} from 'react-native-chat-uikit';
+  useRoomContext,
+} from 'react-native-chat-room';
 
-const appKey = 'easemob#easeim';
-const userId = 'du004';
-const userPassword = '1';
-const usePassword = true; // or false;
-const peerId = 'du005';
+const appKey = '<your app key>';
+const userId = '<current login id>';
+const userName = '<current login name>';
+const userToken = '<current login token or password>';
+const userAvatar = '<current login avatar url>';
+const roomId = '<chat room ID>';
+const room = {
+  roomId: roomId,
+  owner: userId,
+};
 
 function SendMessage() {
   const [page, setPage] = React.useState(0);
   const [_appKey, setAppKey] = React.useState(appKey);
   const [id, setId] = React.useState(userId);
-  const [ps, setPs] = React.useState(userPassword);
-  const [peer, setPeer] = React.useState(peerId);
-  const im = useChatContext();
+  const [ps, setPs] = React.useState(userToken);
+  const im = useRoomContext();
+  const {top} = useSafeAreaInsets();
 
   if (page === 0) {
     return (
-      // 登录页面
-      <SafeAreaView style={{ flex: 1 }}>
+      // Log in page
+      <SafeAreaView style={styles.common}>
         <TextInput
           placeholder="Please App Key."
           value={_appKey}
@@ -48,54 +61,71 @@ function SendMessage() {
           value={ps}
           onChangeText={setPs}
         />
-        <TextInput
-          placeholder="Please peer ID."
-          value={peer}
-          onChangeText={setPeer}
-        />
         <Pressable
+          style={styles.login}
           onPress={() => {
+            // Use a custom avatar and nickname.
             im.login({
               userId: id,
               userToken: ps,
-              usePassword: usePassword,
-              result: (res) => {
+              userNickname: userName,
+              userAvatarURL: userAvatar,
+              result: res => {
                 console.log('login result', res);
                 if (res.isOk === true) {
                   setPage(1);
                 }
               },
             });
-          }}
-        >
+          }}>
           <Text>{'Login'}</Text>
         </Pressable>
         <Pressable
+          style={styles.login}
           onPress={() => {
             im.logout({
               result: () => {},
             });
-          }}
-        >
+          }}>
           <Text>{'Logout'}</Text>
         </Pressable>
       </SafeAreaView>
     );
   } else if (page === 1) {
-    // 聊天页面
+    // chat room page
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <ConversationDetail
-          convId={peer}
-          convType={0}
-          onBack={() => {
-            setPage(0);
-            im.logout({
-              result: () => {},
-            });
+      <SafeAreaView style={styles.common}>
+        <Chatroom
+          messageList={{
+            props: {
+              visible: true,
+              reportProps: {
+                data: [],
+              },
+            },
           }}
-          type={'chat'}
-        />
+          input={{
+            props: {
+              keyboardVerticalOffset: Platform.OS === 'ios' ? top : 0,
+              after: [],
+            },
+          }}
+          roomId={room.roomId}
+          ownerId={room.owner}
+          onError={e => {
+            console.log('ChatroomScreen:onError:', e.toString());
+          }}>
+          <Pressable
+            style={[styles.logout, styles.login]}
+            onPress={() => {
+              setPage(0);
+              im.logout({
+                result: () => {},
+              });
+            }}>
+            <Text>{'log out'}</Text>
+          </Pressable>
+        </Chatroom>
       </SafeAreaView>
     );
   } else {
@@ -104,10 +134,28 @@ function SendMessage() {
 }
 
 export function App(): React.JSX.Element {
-  // 初始化 UIKit
+  // initialize the chat room
   return (
-    <Container options={{ appKey: appKey, autoLogin: false }}>
+    <Container
+      opt={{appKey: appKey, autoLogin: false, debugModel: true} as any}>
       <SendMessage />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  common: {
+    flex: 1,
+  },
+  logout: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  login: {
+    height: 40,
+    backgroundColor: 'darkseagreen',
+    borderColor: 'floralwhite',
+    borderWidth: 1,
+  },
+});
